7f6880cbdff430971e0dee02edd6ec62aed19969
Kerin Cosford <kerin.cosford@digital.justice.gov.uk>
2015-02-11 11:09:45 +0000

    woo heroku

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: DJANGO_SETTINGS_MODULE=vis.settings_heroku gunicorn vis.wsgi --log-file -
+web: gunicorn vis.wsgi --log-file -
--- vis/settings.py
+++ vis/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 import sys
-
+import dj_database_url
 
 # PATH vars
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
@@ -164,3 +164,18 @@ STATIC_URL = '/static/'
 
 MARKDOWN_PROTECT_PREVIEW = True
 
+# Heroku
+DJ_DATABASE_URL = os.environ.get('DATABASE_URL')
+if DJ_DATABASE_URL:
+    DATABASES = {
+        'default': dj_database_url.parse(DJ_DATABASE_URL)
+    }
+
+    PIPELINE_COMPILERS = ()
+    PIPELINE_ENABLED = True
+
+    DEBUG = True
+
+    TEMPLATE_DEBUG = DEBUG
+
+    STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
