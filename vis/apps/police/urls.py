7018802181824c3332122b7a8b35506209697330
Dom Smith <dom@teneightfive.com>
2015-02-13 12:29:42 +0000

    Continue to convert django template to jade

--- vis/apps/police/urls.py
+++ vis/apps/police/urls.py
@@ -6,5 +6,5 @@ from .views import pcc_search, PCCDetail
 urlpatterns = patterns(
     '',
     url(r'^search/$', pcc_search),
-    url(r'^(?P<slug>[a-z-]+)/$', PCCDetail.as_view(), name='pcc_detail')
+    url(r'^(?P<slug>[a-z-]+)/$', PCCDetail.as_view(template_name='police/pcc_detail.jade'), name='pcc_detail')
 )
