9f427a057a790dc7470a68835ce644f1f43733d1
Ravi Kotecha <kotecha.ravi@gmail.com>
2015-02-19 14:04:25 +0000

    change buildpack

--- vis/settings/heroku.py
+++ vis/settings/heroku.py
@@ -16,3 +16,32 @@ DEBUG = False
 TEMPLATE_DEBUG = DEBUG
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': True,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+            'stream': sys.stdout,
+            }
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'DEBUG',
+        'propagate': True,
+        },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'propagate': True,
+            'level': 'DEBUG',
+            },
+        'django.request': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+            'propagate': True,
+            },
+    }
+}
