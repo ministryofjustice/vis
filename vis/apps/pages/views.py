ded1e135d0b435b873116ec80f6b931de086f052
Ravi Kotecha <kotecha.ravi@gmail.com>
2015-02-23 17:32:32 +0000

    add an intermediary search page and some validation and timeouts to the police.uk API

--- vis/apps/pages/views.py
+++ vis/apps/pages/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import get_object_or_404, redirect
-
+from django.core.exceptions import ValidationError
+from django.shortcuts import get_object_or_404, redirect, render
+from requests.exceptions import Timeout
 from pages.models import PCCPage
 
 import requests
@@ -12,17 +13,39 @@ POLICE_URL = ('http://data.police.uk/api/locate-neighbourhood'
 
 
 def pcc_search(request):
+    result = None
+    q = None
+    error = None
     if request.method == 'POST':
         try:
             q = request.POST.get('q')
 
-            geo = requests.get(GEOCODE_URL % q).json()
-            police = requests.get(POLICE_URL % (geo[0]['lat'],
-                                                geo[0]['lon'])).json()
+            geo_resp = requests.get(GEOCODE_URL % q, timeout=5)
+            if not geo_resp.ok:
+                raise ValueError()
 
-            pcc = get_object_or_404(PCCPage, slug=police['force'])
+            geo = geo_resp.json()
+            if len(geo) == 0:
+                raise ValidationError('No results found for %s' % q)
 
-            return redirect(pcc.url)
-        except (KeyError, ValueError):
-            pass
-    return redirect('/')
+            police_resp = requests.get(POLICE_URL % (geo[0]['lat'],
+                                                geo[0]['lon']), timeout=5)
+
+            if not police_resp.ok:
+                if police_resp.status_code == 404:
+                    raise ValidationError("%s isn't a valid postcode in England or Wales" % q)
+                else:
+                    raise ValueError()
+
+            police = police_resp.json()
+
+            result = get_object_or_404(PCCPage, slug=police['force'])
+
+        except ValidationError as ve:
+            error = str(ve.message)
+        except (KeyError, ValueError, Timeout):
+            error = 'something went wrong'
+    return render(request, 'pages/result_list.jade',
+                  {'result': result,
+                   'error': error,
+                   'q': q})
