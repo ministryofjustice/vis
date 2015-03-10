97681404eb54601016379a999042395880fada7a
Dom Smith <dom@teneightfive.com>
2015-03-10 14:37:36 +0000

    Add jade templates for zendesk js module

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,9 @@
 (function(){
   'use strict';
 
-  var gulp = require('gulp'),
-      requireDir = require('require-dir'),
-      dir = requireDir('./tasks'); // load tasks from ./tasks/ folder
+  var gulp = require('gulp');
+  var requireDir = require('require-dir');
+  var dir = requireDir('./tasks'); // load tasks from ./tasks/ folder
 
   // setup default task
   gulp.task('default', ['build:prod']);
