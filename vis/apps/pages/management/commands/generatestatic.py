7b7994909f3c5662f8560e49101d2bd1211137db
Marco Fucci <info@marcofucci.com>
2015-03-12 15:27:11 +0000

    Delete unnecessary folders when generating the static site

--- vis/apps/pages/management/commands/generatestatic.py
+++ vis/apps/pages/management/commands/generatestatic.py
@@ -5,6 +5,21 @@ from django.conf import settings
 from django.core.management.base import NoArgsCommand, CommandError
 from django.core.management import call_command
 
+STATIC_FOLDER_NAME = os.path.basename(settings.STATIC_ROOT)
+FOLDERS_TO_DELETE = [
+    [STATIC_FOLDER_NAME, 'admin'],
+    [STATIC_FOLDER_NAME, 'django_extensions'],
+    [STATIC_FOLDER_NAME, 'fira'],
+    [STATIC_FOLDER_NAME, 'wagtailadmin'],
+    [STATIC_FOLDER_NAME, 'wagtailembeds'],
+    [STATIC_FOLDER_NAME, 'wagtailextra'],
+    [STATIC_FOLDER_NAME, 'wagtailforms'],
+    [STATIC_FOLDER_NAME, 'wagtailsnippets'],
+    [STATIC_FOLDER_NAME, 'wagtailusers']
+]
+
+medusa_path = lambda *x: os.path.join(settings.MEDUSA_DEPLOY_DIR, *x)
+
 
 class Command(NoArgsCommand):
 
@@ -19,9 +34,11 @@ class Command(NoArgsCommand):
 
         # copy static folder
         shutil.copytree(
-            settings.STATIC_ROOT,
-            os.path.join('%s/%s') % (
-                settings.MEDUSA_DEPLOY_DIR,
-                os.path.basename(settings.STATIC_ROOT)
-            )
+            settings.STATIC_ROOT, medusa_path(STATIC_FOLDER_NAME)
         )
+
+        # delete unnecessary folders
+        for folder in FOLDERS_TO_DELETE:
+            shutil.rmtree(
+                medusa_path(*folder)
+            )
