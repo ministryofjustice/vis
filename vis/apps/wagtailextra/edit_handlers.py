433057552ca540d92d430df2a824b21dd5b84935
Marco Fucci <info@marcofucci.com>
2015-03-18 17:20:30 +0000

    cleanup

--- vis/apps/wagtailextra/edit_handlers.py
+++ vis/apps/wagtailextra/edit_handlers.py
@@ -1,4 +1,5 @@
 from django.utils.safestring import mark_safe
+from django.template.loader import render_to_string
 
 from wagtail.wagtailadmin.edit_handlers import BaseFieldPanel
 
@@ -7,13 +8,9 @@ class BaseCoreFieldPanel(BaseFieldPanel):
     field_template = "wagtailextra/edit_handlers/core_field_panel_field.html"
 
     def render_as_field(self, show_help_text=True):
-        from django.template.loader import render_to_string
 
-        instance = self.instance
-        if instance.id:
-            instance = instance.__class__.objects.get(pk=instance.id)
         return mark_safe(render_to_string(self.field_template, {
-            'is_core': instance.is_core,
+            'is_core': self.instance.is_core,
             'field': self.bound_field,
             'field_type': self.field_type(),
             'show_help_text': show_help_text,
