94754204e209cc792dd60254f6bb02aee4b4de80
Marco Fucci <info@marcofucci.com>
2015-03-23 17:00:46 +0000

    Don't show unpublished pcc pages

--- vis/apps/wagtailextra/mixins.py
+++ vis/apps/wagtailextra/mixins.py
@@ -8,10 +8,13 @@ class ObjectListMixin(object):
     object_class = None
     subpage_types = []
 
+    def get_object_list_queryset(self):
+        return self.object_class.objects.all()
+
     def get_context(self, request, *args, **kwargs):
         context = super(ObjectListMixin, self).get_context(
             request, *args, **kwargs
         )
 
-        context['object_list'] = self.object_class.objects.all()
+        context['object_list'] = self.get_object_list_queryset()
         return context
