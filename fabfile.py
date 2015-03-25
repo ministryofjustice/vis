2fffa6560fdf7791a333b18b5a72f6235f5e90c3
Kerin Cosford <kerin.cosford@digital.justice.gov.uk>
2015-03-25 10:15:26 +0000

    Added robots.txt to maintenance page fab task

--- fabfile.py
+++ fabfile.py
@@ -13,3 +13,4 @@ def update_maintenance_page(url):
     with cd('/usr/share/nginx/html'):
         sudo('rm -rf ./*')
         sudo('wget -EHkpnd --user-agent="" -e robots=off %s' % url)
+        sudo("echo 'User-agent: *\nDisallow: /' > robots.txt")
