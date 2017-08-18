79023d517f25e6cd66765ff8e8a89832448a7cd5
Rob Young <rob@robyoung.digital>
2017-08-18 11:51:18 +0100

    Add a deploy script
    This captures the manual steps that we have done to deploy to heroku
    without going through CircleCI.

---
+++ deploy.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+
+if [ $# -ne 1 ]; then
+  >&2 echo "Usage: ./deploy.sh environment"
+  exit 1
+fi
+
+ENVIRONMENT=$1
+GIT_SHA=$(git rev-parse HEAD)
+BRANCH_NAME=$(git rev-parse --abbrev-ref HEAD)
+
+# Check we're deploying the right branch to the right environment
+if [ "$ENVIRONMENT" = "staging" ]; then
+  if [ "$BRANCH_NAME" != "develop" ]; then
+    >&2 echo "Can only deploy 'develop' to staging, you are on '${BRANCH_NAME}'"
+    exit 1
+  fi
+elif [ "$ENVIRONMENT" = "prod" ]; then
+  if [ "$BRANCH_NAME" != "master" ]; then
+    >&2 echo "Can only deploy 'master' to prod, you are on '${BRANCH_NAME}'"
+    exit 1
+  fi
+else
+  >&2 echo "Unknown environment '${ENVIRONMENT}'. Can be prod or staging."
+  exit 1
+fi
+
+# Check we have heroku remotes set up correctly
+if ! git remote | grep "^heroku-$ENVIRONMENT$" > /dev/null; then
+  >&2 echo "Heroku remote not found; expecting heroku-${ENVIRONMENT}"
+  exit 1
+fi
+
+docker build -t moj-vis .
+CONTAINER_ID=$(docker run -d moj-vis)
+
+docker cp $CONTAINER_ID:/app/static ./static
+docker cp $CONTAINER_ID:/app/vis/assets ./vis/assets
+
+git add -f ./static ./vis/assets
+git cm -m 'deploy: add static assets'
+
+git push heroku-${ENVIRONMENT} HEAD:master -f
+heroku config:add GIT_SHA=$GIT_SHA --app vis-${ENVIRONMENT}
+heroku config:add DEPLOY_DATETIME=`date -u +"%Y-%m-%dT%H:%M:%SZ"` --app vis-${ENVIRONMENT}
+git reset --hard HEAD^
