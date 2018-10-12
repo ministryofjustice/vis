43e0d05a772d038ff98473727fb644f441edd8b1
Dom Smith <dom@teneightfive.com>
2018-10-12 15:38:12 +0100

    Remove ruby sass in favour of node sass
    This change removes the need to have Ruby as a dependency for the
    project. Previously it was only being used to compile sass.
    
    Node sass has advanced far enough that it contains enough features
    to be used instead.

--- tasks/sass.js
+++ tasks/sass.js
@@ -3,18 +3,15 @@
 
   var gulp = require('gulp');
   var paths = require('./_paths');
-  var sass = require('gulp-ruby-sass');
+  var sass = require('gulp-sass');
   var browserSync = require('browser-sync');
 
   gulp.task('sass', ['iconfont'], function() {
-    return sass(paths.src + 'stylesheets/', {
-        lineNumbers: true,
-        compass: false,
-        bundleExec: true,
-        style: 'compact',
-        loadPath: ['vis/assets-src/vendor/', 'node_modules/']
-      })
-      .on('error', function (err) { console.log(err.message); })
+    return gulp.src(paths.src + 'stylesheets/**/*.scss')
+      .pipe(sass({
+        outputStyle: 'compact',
+        includePaths: ['vis/assets-src/vendor/', 'node_modules/']
+      }).on('error', sass.logError))
       .pipe(gulp.dest(paths.dest + 'stylesheets/'))
       .pipe(browserSync.reload({stream:true}));
   });
