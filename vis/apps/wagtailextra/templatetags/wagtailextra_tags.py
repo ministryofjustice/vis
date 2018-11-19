f69782978c62c2abcbd73a70fc0be71ac20fba5e
Dom Smith <dom@teneightfive.com>
2018-11-19 12:45:22 +0000

    Add an ID to each heading in content blocks
    This change allows anchor links to be created to specific bits of
    content within a long page.
    
    At the moment there is no way to generate a link to a manual anchor
    within the richtext editor.
    
    This slugifys the text of a heading and uses this value as the ID.

--- vis/apps/wagtailextra/templatetags/wagtailextra_tags.py
+++ vis/apps/wagtailextra/templatetags/wagtailextra_tags.py
@@ -1,4 +1,7 @@
 from django import template
+from django.utils.safestring import mark_safe
+from django.utils.text import slugify
+from bs4 import BeautifulSoup, Tag
 
 register = template.Library()
 
@@ -6,3 +9,20 @@ register = template.Library()
 @register.filter
 def is_pcc_user(user):
     return user.groups.filter(name='PCCEditor').exists()
+
+@register.filter
+def heading_ids(value):
+    """
+    Picks out the headings of a html string and assigns an ID
+    to each one so that they can be linked to using an anchor link
+    """
+    soup = BeautifulSoup(value, 'html5lib')
+    headings = soup.find_all(['h1', 'h2', 'h3', 'h4', 'h5', 'h6'])
+
+    for heading in headings:
+        heading['id'] = slugify(heading.getText())
+
+    soup.html.unwrap()
+    soup.head.unwrap()
+    soup.body.unwrap()
+    return mark_safe(soup.prettify())
