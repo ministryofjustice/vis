8b796c6ec87fb866a4f757b77108fc41ae97b904
Dom Smith <dom@teneightfive.com>
2019-01-08 11:40:57 +0000

    Add support to set a primary domain
    As the service has moved domain we want to redirect all traffic
    to the old domain to the new domain including the full URL path.
    
    This change will redirect traffic if an environment variable has been
    set with a primary domain if the requested domain doesn't match.

--- vis/apps/core/middleware.py
+++ vis/apps/core/middleware.py
@@ -1,4 +1,5 @@
 from django.conf import settings
+from django.shortcuts import redirect
 
 # default 30 days
 MAX_AGE = getattr(settings, 'CACHE_CONTROL_MAX_AGE', 2592000)
@@ -15,3 +16,8 @@ class MaxAgeMiddleware(object):
 
         response['Cache-Control'] = 'max-age=%d' % MAX_AGE
         return response
+
+class DomainRedirect(object):
+    def process_request(self, request):
+        if settings.PRIMARY_DOMAIN and (request.get_host() not in settings.PRIMARY_DOMAIN):
+            return redirect(settings.PRIMARY_DOMAIN + request.get_full_path(), True)
